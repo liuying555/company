134dd8a675bc2e3dd71f96a03a91f9b1bbf2f053
liuying555 <247220246@qq.com>
2018-05-31 00:07:31 +0800

    忘记提交的

---
+++ src/app/layout/layer/layer.component.ts
@@ -0,0 +1,25 @@
+
+import {Component, ElementRef, OnInit, TemplateRef, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-layer',
+  templateUrl: './layer.component.html',
+  styleUrls: ['./layer.component.css']
+})
+export class LayerComponent implements OnInit {
+  isCollapsed = false;
+  triggerTemplate = null;
+  @ViewChild('trigger') customTrigger: TemplateRef<void>;
+
+  /** custom trigger can be TemplateRef **/
+  changeTrigger(): void {
+    this.triggerTemplate = this.customTrigger;
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
