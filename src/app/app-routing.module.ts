b6a66f2ff862f529349d99704bd082c9fb4c842b
liuying555 <247220246@qq.com>
2018-05-31 00:05:46 +0800

    基础文件

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SiderbarComponent } from './layout/siderbar/siderbar.component';
+import { ContentComponent } from './layout/content/content.component';
+import { LayerComponent } from './layout/layer/layer.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/layer', pathMatch: 'full' },
+  { path: 'layer', component: LayerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SiderbarComponent } from './layout/siderbar/siderbar.component';
+import { ContentComponent } from './layout/content/content.component';
+import { LayerComponent } from './layout/layer/layer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SiderbarComponent,
+    ContentComponent,
+    LayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
